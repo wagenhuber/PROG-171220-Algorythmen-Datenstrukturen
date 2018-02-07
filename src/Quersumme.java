98bc4df20453256a2b37d86481588cf84ad9ef42
wagenhuber <wagenhuber@gmx.net>
2018-02-07 09:55:16 +0100

    inital commit

---
+++ src/Quersumme.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Quersumme {
+
+    public static void main(String[] args) {
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Bitte ganze Zahl eingeben: ");
+        int eingabe = scanner.nextInt();
+        scanner.close();
+
+        String trenner = ((Integer) eingabe).toString();
+
+
+        int quersumme;
+
+
+
+        System.out.println("Die Quersumme lautet:");
+
+
+    }
+
+}
