a58ef761abecde808d3c80c7db3a2f1cb4b04770
wagenhuber <wagenhuber@gmx.net>
2018-05-02 11:13:14 +0200

    Ergänzung Korrektheit

---
+++ src/Korrektheit.java
@@ -0,0 +1,11 @@
+public class Korrektheit {
+
+    //Stichwort: Schleifeninvariante
+    //partielle Korrektheit
+    //Totale Korrektheit (= partielle Korrektheit + Terminierung)
+
+    //Terminierung Bsp: In jeden Durchlauf wird int y reduziert => irgendwann y = 0 => Schleifenende/Programmende
+
+    //In Prüfung: Zusicherung anhand einfachen Beispiel aufschreiben können!
+
+}
